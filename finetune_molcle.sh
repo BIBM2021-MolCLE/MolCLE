8109ce3444a0b5514a33c0e5b5e654d55c574765
CIKM2021-MolCLE <84886710+CIKM2021-MolCLE@users.noreply.github.com>
2021-05-27 14:24:10 +0800

    Update finetune_molcle.sh

--- finetune_molcle.sh
+++ finetune_molcle.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m finetune_pgcl train_GC --dataset_name bace --reward_method mc_l_shapley --input_model_file "./models_molCLE/molCLE_zinc_standard_agent_11.pth"
+python -m finetune_molcle train_GC --dataset_name bace --reward_method mc_l_shapley --input_model_file "./models_molCLE/molCLE_zinc_standard_agent_11.pth"
